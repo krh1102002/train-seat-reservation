f50a69ca59388837a29785328206b2f00c77fe95
Kunal Hulke <hulke1102002@gmail.com>
2024-08-21 03:31:58 +0530

    resolving the errors

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,16 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const mongoURI =
-  "mongodb+srv://hulke123:hulke123@cluster0.su2u9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const mongoURI = process.env.MONGODB_URI;
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
@@ -64,11 +63,9 @@ app.post("/api/book", async (req, res) => {
     const { numSeats } = req.body;
 
     if (numSeats < 1 || numSeats > 7) {
-      return res
-        .status(400)
-        .json({
-          message: "Invalid number of seats. Please book 1 to 7 seats.",
-        });
+      return res.status(400).json({
+        message: "Invalid number of seats. Please book 1 to 7 seats.",
+      });
     }
 
     const availableSeats = await Seat.find({ isBooked: false }).sort({
